8b93488ef73dfe2f70699992dd743fffd772bac2
Richard David <driichard@gmail.com>
2019-03-28 22:41:11 -0500

    add a reduxexample project

---
+++ reduxexample/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Posts from './components/Posts'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <Posts />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
