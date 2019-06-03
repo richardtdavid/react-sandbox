e21ab48945da338b3efbc7812d8b7950b13b6688
Richard David <driichard@gmail.com>
2019-06-03 01:35:20 -0500

    associating streams with users

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducers from './streamReducers'
 
 
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducers
 });
--- streams/client/src/reducers/streamReducers.js
+++ streams/client/src/reducers/streamReducers.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -13,11 +14,11 @@ export default (state = {}, action) => {
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload } 
         case FETCH_STREAMS:
-            return { ...state, [action.payload.id]: action.payload } 
+            return { ...state, ..._.mapKeys(action.payload, 'id') } 
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload } 
         case DELETE_STREAM:
-            return { ...state, [action.payload.id]: action.payload } 
+            return _.omit(state, action.payload)
         default:
             return state;
     }   
