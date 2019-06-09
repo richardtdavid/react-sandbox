f8a30ef468088c597e052850ca9d0677496d679d
Richard David <driichard@gmail.com>
2019-06-09 14:07:50 -0500

    started streaming aspect part

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
-const StreamShow = () => {
-  return (
-    <div>
-      StreamShow
-    </div>
-  );
+class StreamShow extends React.Component {
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render(){
+    if(!this.props.stream){
+      return <div>Loading</div>
+    }
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h4>{description}</h4>
+      </div>
+    );
+  }
 }
 
-export default StreamShow;
+const mapStateToprop = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(
+  mapStateToprop,
+   { fetchStream }
+)(StreamShow);
