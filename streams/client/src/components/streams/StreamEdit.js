eb73f3ad990ca5f7e6cf685aec026c8408bd35f9
Richard David <driichard@gmail.com>
2019-06-08 23:33:25 -0500

    Streams can now be delete

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,25 @@
+import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './streamForm'
 
 class StreamEdit extends React.Component {
   componentDidMount(){
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  }
+
   render(){
     return (
       <div>
-        StreamEdit
+        <h3>Edit a Stream</h3>
+        <StreamForm 
+          initialValues={_.pick(this.props.stream, 'title', 'description')}  
+          onSubmit={this.onSubmit} />
       </div>
     );
   }
@@ -22,5 +31,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps, 
-  { fetchStream }
+  { fetchStream, editStream }
   )(StreamEdit);
