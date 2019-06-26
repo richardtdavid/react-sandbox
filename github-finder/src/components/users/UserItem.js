996d47b6674b5fea34d2bc2fd16188d567de4906
Richard David <driichard@gmail.com>
2019-06-26 00:35:40 -0500

    new components and created api keys from github

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-    render() {
-        const { login, avatar_url, html_url} = this.props.users;
+const UserItem = ({users: { login, avatar_url, html_url}}) => {
         return (
             <div className='card text-center'>
                 <img src={avatar_url} alt="" className='round-img' style={{width: '60px'}}/>
@@ -12,7 +11,10 @@ class UserItem extends Component {
                 </div>
             </div>
         )
-    }
+}
+
+UserItem.prototype = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
