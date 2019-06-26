40b9ee7a64f9972d76a5d0d286e883be3ebbfd1a
Richard David <driichard@gmail.com>
2019-06-25 23:05:27 -0500

    created a user component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Navbar title='Github Finder' icon="fab fa-github"/>
+        <Navbar />
+        <div className="container">
+          <Users />  
+        </div>
       </div>
     );
   }
