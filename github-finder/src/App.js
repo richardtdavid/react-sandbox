d98eb89c34d883131269095d815369c1d27fdcb0
Richard David <driichard@gmail.com>
2019-06-25 22:29:42 -0500

    created new project

---
+++ github-finder/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title='Github Finder' icon="fab fa-github"/>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
