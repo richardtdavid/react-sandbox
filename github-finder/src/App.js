9fdc1ddf1a63fbdb8ca87b2251dbaf982092d243
Richard David <driichard@gmail.com>
2019-06-26 22:12:54 -0500

    alert state & components

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,27 +2,54 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
 import axios from 'axios'
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
-  
-  async componentDidMount(){
-    this.setState({loading: true});
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users: res.data, loading: false});
 
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({loading: false});
+
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data.items, loading: false});
   }
+
+  // Clear users from state 
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  }
+
+  // Alert user when searchbox is empty
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }});
+
+    setTimeout(() => {
+      this.setState({alert: null});
+    }, 5000);
+  }
+
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>  
+          <Alert alert={this.state.alert}/> 
+          <Search searchUsers={this.searchUsers} 
+          clearUsers={this.clearUsers} 
+          showClear={users.length > 0 ? true : false}
+          setAlerts={this.setAlert}/>
+
+          <Users loading={loading} users={users}/>  
         </div>
       </div>
     );
