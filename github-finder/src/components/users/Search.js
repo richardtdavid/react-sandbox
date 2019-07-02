52273731cde0f153483ad6c77ea17117acac92f0
Richard David <driichard@gmail.com>
2019-07-01 22:55:53 -0500

    refactored to use Context API

--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useContext } from 'react'
+import GithubContext from '../../context/github/githubContext'
+import AlertContext from '../../context/alert/alertContext'
 
 
-const Search = ({searchUsers, showClear, clearUsers, setAlerts}) => {
+const Search = () => {
+    const alertContext = useContext(AlertContext);
+    const githubContext = useContext(GithubContext);
+
+    const { setAlerts } = alertContext;
+
     const [text, setText] = useState('');
     const onChange = e => setText( e.target.value );
     const onSubmit = e => {
@@ -11,7 +17,7 @@ const Search = ({searchUsers, showClear, clearUsers, setAlerts}) => {
         if(text === ''){
             setAlerts('Please enter something', 'light')
         }else{
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText("");
         }
     }
@@ -25,16 +31,10 @@ const Search = ({searchUsers, showClear, clearUsers, setAlerts}) => {
                     onChange={onChange}/>
                 <input type="submit" value="Search" className="btn btn-dark btn-block"/>
             </form>       
-            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers} >Clear</button>}
+            {githubContext.users.length > 0  && <button className="btn btn-light btn-block" onClick={githubContext.clearUsers} >Clear</button>}
         </div>
     )
 }
 
-Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlerts: PropTypes.func.isRequired,
-}
 
 export default Search
--- github-finder/src/components/users/User.js
+++ github-finder/src/components/users/User.js
@@ -1,12 +1,15 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
-import PropTypes from 'prop-types'
+import GithubContext from '../../context/github/githubContext'
 
 
+const User = ({match}) => {
+    const githubContext = useContext(GithubContext)
 
-const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+    const { getUser, loading, user, getUserRepos, repos } = githubContext;
+
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);   
@@ -92,12 +95,5 @@ const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
     )
 }
 
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired,
-}
 
 export default User
