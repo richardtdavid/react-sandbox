24a68fab1943d350632f6f01e115b44bf6e9943e
Richard David <driichard@gmail.com>
2019-08-12 22:20:17 -0500

    add context and pages

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -19,6 +19,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5010;
 
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
